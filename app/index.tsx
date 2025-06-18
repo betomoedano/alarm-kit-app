b71866e945228f0cd2fc2bd68626e889eb0715ad
betomoedano <betomoedano@outlook.com>
2025-06-18 17:48:29 -0500

    refactor alarm permission handling; remove loading state from layout and implement it in index screen with user feedback for denied permissions

--- app/index.tsx
+++ app/index.tsx
@@ -1,21 +1,57 @@
-import { Button, Text, View } from "react-native";
+import {
+  ActivityIndicator,
+  Button,
+  StyleSheet,
+  Text,
+  View,
+} from "react-native";
 import ExpoAlarmKit, { AlarmPermissionStatus } from "@/modules/expo-alarm-kit";
 import { useEffect, useState } from "react";
 
 export default function Index() {
+  const [isLoading, setIsLoading] = useState(true);
   const [alarmPermissionStatus, setAlarmPermissionStatus] =
     useState<AlarmPermissionStatus>("notDetermined");
 
+  useEffect(() => {
+    getAlarmPermissionStatus();
+  }, []);
+
+  async function getAlarmPermissionStatus() {
+    try {
+      const status = await ExpoAlarmKit.getAlarmPermissionsAsync();
+      setAlarmPermissionStatus(status);
+    } catch (error) {
+      console.error(error);
+    } finally {
+      setIsLoading(false);
+    }
+  }
+
+  if (isLoading) {
+    return (
+      <View style={styles.container}>
+        <ActivityIndicator />
+      </View>
+    );
+  }
+
+  if (alarmPermissionStatus === "denied") {
+    return (
+      <View style={styles.container}>
+        <Text>
+          Permission denied, please go to settings and enable permissions.
+        </Text>
+      </View>
+    );
+  }
+
   return (
-    <View
-      style={{
-        flex: 1,
-        justifyContent: "center",
-        alignItems: "center",
-      }}
-    >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
-      <Text>{alarmPermissionStatus}</Text>
+    <View style={styles.container}>
+      <Text>
+        Alarm Permission Status:{" "}
+        <Text style={{ fontWeight: "bold" }}>{alarmPermissionStatus}</Text>
+      </Text>
       <Button
         title="Schedule One Off Alarm"
         onPress={() => {
@@ -33,3 +69,11 @@ export default function Index() {
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+  },
+});
